3b62ae5832e7d4439720c7b41068278e24935bf2
Monicah Gikanga <mgikanga@Admins-MacBook-Air.local>
2020-06-09 08:44:28 -0700

    added server

---
+++ build/precache-manifest.c2a2cb1a8e99e66a32defdb0bb505df4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde983aaca4bcc8e8a22607890141814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbba617b030a0a81de77",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d265019208a4a1ba188c",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cbba617b030a0a81de77",
+    "url": "/static/js/2.f0cee2f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f0cee2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d265019208a4a1ba188c",
+    "url": "/static/js/main.f301294e.chunk.js"
+  },
+  {
+    "revision": "45cfe685c796e256e7ac",
+    "url": "/static/js/runtime-main.faf5df49.js"
+  }
+]);
